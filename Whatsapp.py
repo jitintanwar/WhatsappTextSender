4e2282c6540a1d4448cb4ccc675369415c40cc8b
Jitin Tanwar <tanwarjitin@gmail.com>
2019-10-10 10:19:48 +0530

    media feature added

--- Whatsapp.py
+++ Whatsapp.py
@@ -3,6 +3,39 @@
 
 from selenium import webdriver
 import random
+from time import sleep
+
+
+def sendText():
+    for name in contact_names:
+        user = driver.find_element_by_xpath(
+            '//span[@title = "{}"]'.format(name))
+        user.click()
+        msg_box = driver.find_element_by_class_name('_3FeAD')
+        msg_box.send_keys(random.choice(msg))
+        btn = driver.find_element_by_class_name('_3M-N-')
+        btn.click()
+
+
+def sendMedia():
+    filepath = r'\path\to\your\media.jpg'
+    for name in contact_names:
+        user = driver.find_element_by_xpath(
+            '//span[@title = "{}"]'.format(name))
+        user.click()
+        attach_btn = driver.find_element_by_xpath('//div[@title="Attach"]')
+        attach_btn.click()
+
+        photos_btn = driver.find_element_by_xpath(
+            '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+        photos_btn.send_keys(filepath)
+
+        sleep(2)
+
+        send_btn = driver.find_element_by_xpath(
+            '//span[@data-icon="send-light"]')
+        send_btn.click()
+
 
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com')
@@ -16,11 +49,7 @@ for name in webElements:
 msg = ["Hope you are doing well.",
        "I hope you have a good day.", "Greetings for the day."]
 
+sleep(5)
 
-for name in contact_names:
-    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-    user.click()
-    msg_box = driver.find_element_by_class_name('_3FeAD')
-    msg_box.send_keys(random.choice(msg))
-    btn = driver.find_element_by_class_name('_3M-N-')
-    btn.click()
+sendText()
+# sendMedia()
